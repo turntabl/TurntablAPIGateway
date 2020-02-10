e8972815ba9205248255a2a97736745840c7d584
idawud <ismaildawud96@gmail.com>
2020-02-10 13:53:24 +0000

    docker compose for services

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+
+FROM adoptopenjdk:11-jre-hotspot
+
+COPY build/libs/TurntablAPIGateway-0.0.1-SNAPSHOT.jar   app.jar
+RUN chmod 777 app.jar
+
+ENTRYPOINT [ "java", "-jar", "app.jar" ]
--- src/main/java/io/tuntabl/TurntablApiGatewayApplication.java
+++ src/main/java/io/tuntabl/TurntablApiGatewayApplication.java
@@ -2,6 +2,9 @@ package io.tuntabl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TurntablApiGatewayApplication {
@@ -9,5 +12,19 @@ public class TurntablApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TurntablApiGatewayApplication.class, args);
 	}
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("hello", r ->
+                        r.host("hello.**")
+                       .filters(f -> f.rewritePath("/hello/(?<segment>.*)", "/${segment}"))
+                        .uri("http://hello-service:5000"))
+                .route("todo",
+                        r -> r.host("todo.**")
+                        .filters(f -> f.rewritePath("/todo/(?<segment>.*)", "/${segment}"))
+                        .uri("http://todo-service:8080"))
+                .build();
+    }
 
 }
